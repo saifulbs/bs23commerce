8a8c8facc977e8317c96b179fd1988154e209f70
Shaishab <shaishab.cse@gmail.com>
2015-06-08 20:39:56 +0600

    update user route to call some functions according to route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,36 +1,23 @@
 'use strict'
 
-var express = require('express');
+var express = require('express'),
+    userController = require('./controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('user');
-});
+router.get('/', userController.getUser);
 
-router.get('/:userId', function(req, res) {
-  res.send('user');
-});
+router.get('/:userId', userController.getUserById);
 
-router.post('/', function(req, res) {
-  res.send('user');
-});
+router.post('/', userController.createUser);
 
-router.put('/', function(req, res) {
-  res.send('user');
-});
+router.put('/', userController.updateUser);
 
-router.delete('/:userId', function(req, res) {
-  res.send('user');
-});
+router.delete('/:userId', userController.deleteUserById);
 
-router.put('/changePassword', function(req, res) {
-  res.send('user');
-});
+router.put('/changePassword', userController.changePassword);
 
-router.put('/resetForgotPassword', function(req, res) {
-  res.send('user');
-});
+router.put('/resetForgotPassword', userController.resetForgotPassword);
 
 router.post('/login', function(req, res) {
   res.send('user');
